e7fa393a07743359e2a94fa24a4ba0ad80eed3ef
79234 <marusya0323@gmail.com>
2023-11-08 10:16:35 +0700

    добавили необходимые маршруты

--- tutorial/tutorial/urls.py
+++ tutorial/tutorial/urls.py
@@ -20,3 +20,8 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+from django.urls import path, include
+
+urlpatterns = [
+   path('api/', include('snippets.urls')),
+]
