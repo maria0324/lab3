699e97c5b31ab096ff88646fd9ab4f0077cfa313
79234 <marusya0323@gmail.com>
2023-11-08 10:42:31 +0700

    Изменили функцию snippet_list

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -8,8 +8,15 @@ from rest_framework.parsers import JSONParser
 from .models import Snippet
 from .serializers import SnippetSerializer
 
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-@csrf_exempt
+from .models import Snippet
+from .serializers import SnippetSerializer
+
+
+@api_view(['GET', 'POST'])
 def snippet_list(request):
    """
    List all code snippets, or create a new snippet.
@@ -17,15 +24,16 @@ def snippet_list(request):
    if request.method == 'GET':
        snippets = Snippet.objects.all()
        serializer = SnippetSerializer(snippets, many=True)
-       return JsonResponse(serializer.data, safe=False)
+       return Response(serializer.data)
 
    elif request.method == 'POST':
-       data = JSONParser().parse(request)
-       serializer = SnippetSerializer(data=data)
+       serializer = SnippetSerializer(data=request.data)
        if serializer.is_valid():
            serializer.save()
-           return JsonResponse(serializer.data, status=201)
-       return JsonResponse(serializer.errors, status=400)
+           return Response(serializer.data, status=status.HTTP_201_CREATED)
+       return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
 
 
 @csrf_exempt
